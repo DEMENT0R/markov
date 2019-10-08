ed695526302443128ae187a8ac2b49e8a58edd96
Maxim Bezrukavnikov <bms@935914.ru>
2019-10-08 15:03:50 +1000

    Changes...

--- markov.php
+++ markov.php
@@ -6,9 +6,15 @@ header('Content-Type: text/html; charset=utf-8');
 
 require_once("class_markov.php");
 
-$data = file_get_contents("tz.txt");
+$data = file_get_contents("text.txt");
 
-$mk = new \Generator\Markov($data,15);
-$text = $mk->get_result();
+$mk = new \Generator\Markov($data,5);
+$text = "<p>".$mk->get_result();
+
+$mk = new \Generator\Markov($data,5);
+$text .= "</p><p>".$mk->get_result();
+
+$mk = new \Generator\Markov($data,5);
+$text .= "</p><p>".$mk->get_result();
 
 echo "<p style='word-wrap: break-word;'>".$text."</p>";
